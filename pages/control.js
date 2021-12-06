5928a161bfa5363c2b4bbc4238c1bf5570b3c6f2
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-12-07 01:38:14 +0700

    Add pages/control.js

---
+++ pages/control.js
@@ -0,0 +1,11 @@
+import BottomNav from '../components/bottomNav';
+
+const Control = () => {
+  return (
+		<div className="w-full h-screen">
+			<BottomNav />
+		</div>
+  );
+}
+
+export default Control;
