5d6e750eee8e183d8cc9e75e6277300038c70a5c
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-12-07 16:57:32 +0700

    Add Card Component

---
+++ components/Card.js
@@ -0,0 +1,15 @@
+const Card = ({icon, title, value, unit}) => {
+  return (
+    <div className="bg-white  rounded-lg shadow flex flex-cols px-4 py-5 items-center">
+      <div className="text-3xl">
+        <i className={'bi ' + icon}></i>
+      </div>
+      <div className="pl-3">
+        <div className="font-semibold">{ title }</div>
+        <div className="text-sm">{ value } { unit }</div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
