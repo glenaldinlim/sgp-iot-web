24e7119f8b1891232ad75ae5bfe387784409cdaa
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-12-10 17:58:51 +0700

    Modify Index Page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 import Head from 'next/head';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { ref, onValue } from "firebase/database";
 
 import BottomNav from "../components/bottomNav";
 import Card from "../components/Card";
-import Modal from "../components/Modal";
+
+import { db } from '../firebase';
+import CardSkeleton from '../components/skeleton/CardSkeleton';
 
 const Home = () => {
-  const [open, setOpen] = useState(false)
-  
-  const onChangeOpen = () => {
-    setOpen(!open)
-  }
+  const [monitoring, setMonitoring] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const monitoringRef = ref(db, '/v1/monitoring')
+    const unsubcribe = onValue(monitoringRef, (snapshot) => {
+      setMonitoring(snapshot.val())
+      setLoading(false)
+    })
+
+    return unsubcribe
+  }, [])
 
   return (
     <div className="w-full min-h-screen bg-gray-50">
@@ -22,39 +32,32 @@ const Home = () => {
         Home
       </h1>
       <main className="mb-auto">
-        <div className="grid grid-cols-2 gap-2 px-2">
-          <Card icon="bi-thermometer-snow" title="Temperature" value="30.0" unit="C" />
-          <Card icon="bi-cloud-fog2" title="Humidity" value="30.1" unit="%" />
-          <Card icon="bi-moisture" title="Soil Moisture" value="30.1" unit="%" />
-          <Card icon="bi-droplet" title="Height" value="5" unit="cm" />
-        </div>
-        <p className="text-xs text-center mt-3 text-gray-600">
-          Update Terakhir: 05 Des 2021 08:10
-        </p>
-        <hr className="my-5" />
-        <div className="flex justify-center px-2 mb-5">
-          <table className="border-collapse border border-gray-400 table-auto">
-            <thead>
-              <tr className="bg-gray-200">
-                <th className="py-1 px-2">Tanggal</th>
-                <th className="py-1 px-2">Temperature</th>
-                <th className="py-1 px-2">Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="border border-gray-400">
-                <td className="p-1">04/12/2021 18:50</td>
-                <td className="p-1">30.1 C</td>
-                <td className="p-1">
-                  <button className="bg-green-400 hover:bg-green-500 px-3 rounded-md" onClick={() => setOpen(true)}>
-                    <i className="bi bi-eye-fill text-white hover:text-gray-100"></i>
-                  </button>
-                </td>
-                { open ? <Modal open={onChangeOpen} /> : null }
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        {
+          loading ?
+            <>
+              <div className="grid grid-cols-1 gap-4 px-10">
+                <CardSkeleton />
+                <CardSkeleton />
+                <CardSkeleton />
+                <CardSkeleton />
+              </div>
+              <div className="animate-pulse flex justify-center mt-4">
+                <div className="h-4 bg-gray-200 rounded w-2/4"></div>
+              </div>
+            </>
+          : 
+            <>
+              <div className="grid grid-cols-1 gap-4 px-10">
+                <Card icon="bi-thermometer-snow" title="Temperature" value={monitoring.temperature} unit="C" />
+                <Card icon="bi-cloud-fog2" title="Humidity" value={monitoring.humidity} unit="%" />
+                <Card icon="bi-moisture" title="Soil Moisture" value={(monitoring.soilMoistureA + monitoring.soilMoistureB) / 2} unit="%" />
+                <Card icon="bi-droplet" title="Height" value={monitoring.waterHeight} unit="cm" />
+              </div>
+              <p className="text-xs text-center mt-3 text-gray-600">
+                Update Terakhir: {monitoring.updated_at}
+              </p>
+            </>
+        }
       </main>
       <BottomNav />
     </div>
