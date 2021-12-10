1f2c4c08c7a2e9adef01ce679379049693ff00c7
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-12-10 17:58:12 +0700

    Modify Card Component

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,12 @@
 const Card = ({icon, title, value, unit}) => {
   return (
     <div className="bg-white rounded-lg shadow flex px-4 py-5 items-center">
-      <div className="text-2xl">
+      <div className="text-3xl">
         <i className={'bi ' + icon}></i>
       </div>
-      <div className="pl-3">
-        <div className="font-semibold text-sm">{ title }</div>
-        <div className="text-xs">{ value } { unit }</div>
+      <div className="pl-4">
+        <div className="font-semibold">{ title }</div>
+        <div className="text-sm">{ value } { unit }</div>
       </div>
     </div>
   )
